7023b7fffea77c95a9db3622079aaed14681ea4b
lzz <447089649@qq.com>
2018-08-15 20:27:46 +0800

    登录视图login.vue

---
+++ src/views/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <el-form label-position="top" label-width="80px" :model="formData">
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formData.password"></el-input>
+      </el-form-item>
+    </el-form>
+      <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
