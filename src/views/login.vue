15dcbd7dd0cec4931307a78a91870cef42ca8be2
lzz <447089649@qq.com>
2018-08-15 20:48:50 +0800

    login.vue样式调整

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
-    <el-form label-position="top" label-width="80px" :model="formData">
+  <div class="login-wrap">
+
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formData">
+      <h2>用户登录</h2>
       <el-form-item label="用户名">
         <el-input v-model="formData.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
         <el-input v-model="formData.password"></el-input>
       </el-form-item>
+      <el-button class="login-button" type="primary">登录</el-button>
     </el-form>
-      <el-button type="primary">主要按钮</el-button>
   </div>
 </template>
 
@@ -26,4 +28,23 @@ export default {
 </script>
 
 <style>
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-wrap .login-form {
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+.login-wrap .login-form .login-button {
+  width: 100%;
+}
+
 </style>
