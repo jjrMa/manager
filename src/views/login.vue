05512964017e005658a97931b05c9329a5830ce7
lzz <447089649@qq.com>
2018-08-16 12:52:59 +0800

    登录功能完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -7,9 +7,9 @@
         <el-input v-model="formData.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
-        <el-input v-model="formData.password"></el-input>
+        <el-input type="password" v-model="formData.password"></el-input>
       </el-form-item>
-      <el-button class="login-button" type="primary">登录</el-button>
+      <el-button @click="handleLogin" class="login-button" type="primary">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -22,6 +22,22 @@ export default {
         username: '',
         password: ''
       }
+    }
+  },
+  methods: {
+    handleLogin () {
+      this.$http.post('login', this.formData)
+        .then((res) => {
+          const data = res.data
+          const {meta: {status, msg}} = data
+          if (status === 200) {
+            const token = data.data.token
+            sessionStorage.setItem('token', token)
+            this.$message.success(msg)
+          } else {
+            this.$message.error(msg)
+          }
+        })
     }
   }
 }
