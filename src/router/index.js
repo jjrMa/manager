1f4f5322f645d9cd0282c7db81a981bffea1f878
lzz <447089649@qq.com>
2018-08-16 13:28:00 +0800

    home组件布局

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(Router)
 
@@ -9,7 +10,7 @@ export default new Router({
   routes: [{
     name: 'home',
     path: '/',
-    redirect: { name: 'login' }
+    component: Home
   },
   {
     name: 'login',
