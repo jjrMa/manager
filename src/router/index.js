ad07c7135cf1b0bbb524a5b679ac234f0ef7d733
jjrMa <jjr_ma@163.com>
2019-01-05 19:34:00 +0800

    dev-user

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/views/login'
 import Home from '@/views/home'
+import User from '@/views/user/user'
 
 Vue.use(Router)
 
@@ -9,7 +10,12 @@ export default new Router({
   routes: [{
     name: 'home',
     path: '/',
-    component: Home
+    component: Home,
+    children: [{
+      name: 'user',
+      path: '/user',
+      component: User
+    }]
   },
   {
     name: 'login',
